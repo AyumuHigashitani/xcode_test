17648f68eeebb458d98e03b1e9c61845737e86e3
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2019-11-14 11:21:51 +0900

    test

--- test_bottun/GameViewController.swift
+++ test_bottun/GameViewController.swift
@@ -7,44 +7,46 @@
 //
 
 import UIKit
-import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+    
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    
+  
+    @IBAction func button1(_ sender: Any) {
+        label1.text = ""
+        label2.text = "hellow"
     }
-
-    override var shouldAutorotate: Bool {
-        return true
+    
+    @IBAction func button2(_ sender: Any) {
+        label1.text = "hellow"
+        label2.text = ""
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    /*
+    @IBOutlet var labeltest :UILabel!
+    @IBOutlet var buttonTest :UIButton!
+    
+       var count = true
+    
+       override func viewDidLoad() {
+           super.viewDidLoad()
+    
+           labeltest.text = "Swift Test"
+    
+           buttonTest.setTitle("Button",for:UIControl.State.normal)
+    
+       }
+    
+       @IBAction func buttonTapped(_ sender : Any) {
+           if(count){
+               labeltest.text = "Swift Test"
+                count = false
+           }
+           else{
+               labeltest?.text = "tapped !"
+                count = true
+           }
+       }
+*/
 }
