7d2630bf5e38e7cb01396e30cb7e1c615fa5c99a
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2019-12-15 00:10:28 +0900

    aaassss

--- test_bottun/GameViewController.swift
+++ test_bottun/GameViewController.swift
@@ -23,8 +23,8 @@ class GameViewController: UIViewController {
     //ここ開けました
     
     //ここ開けました　part2
-    
     //ここ開けました　part3
+    //ここ開けました　part4
     @IBAction func button2(_ sender: Any) {
         label1.text = "hello, world2"
         label2.text = ""
