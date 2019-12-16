5a4d2ac9c7169ae0f7fdab3663df1b98e2c33c45
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2019-12-16 21:32:33 +0900

    aaaa

---
+++ test_bottun/Higashitani.swift
@@ -0,0 +1,12 @@
+//
+//  Higashitani.swift
+//  test_bottun
+//
+//  Created by 東谷　歩夢 on 2019/12/16.
+//  Copyright © 2019 東谷　歩夢. All rights reserved.
+//
+
+import Foundation
+
+class higasitani {
+}
